6fc02be7321b0c76470e31afe02023a32a64c33b
Arima928 <arimasrivastava16@gmail.com>
2025-06-13 23:24:11 +0530

    this is my changed commit message
    :qa!

--- butterfly.java
+++ butterfly.java
@@ -5,6 +5,48 @@ public class butterfly {
         
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
+
+      //upper half
+        for(int i =1;i<=n;i++)
+
+        //1st half
+        {
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+
+            }
+            //spaces
+            int spaces=2*(n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd half
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+//lower half
+        for(int i=n; i>=1; i--){
+
+            //1st half
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            //spaces
+            int spaces=2*(n-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+            }
+            //2nd half
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        
         
     }
 }
